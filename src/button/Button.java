c30e69af57a864d08ec4a54cbe60668aa9bf4089
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2016-12-20 12:52:46 +0100

    add button press functionality

--- src/button/Button.java
+++ src/button/Button.java
@@ -1,15 +1,30 @@
 package button;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 
 import lampButtonServer.ButtonInterface;
+import lampButtonServer.Server;
 
 public class Button implements ButtonInterface {
 
 	@Override
 	public void press() throws RemoteException {
-		// TODO Auto-generated method stub
-		
+		try {
+			Server s = (Server) Naming.lookup("rmi://localhost:1099/Server"); //Proxy-Object
+			s.sendMessage("press");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
+	public static void main(String[] args) {
+		Button b = new Button();
+		try {
+			b.press();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
